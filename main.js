4917af0542fbd8458510504e1125d3e7a67cf8f2
Georgette-Turner <63122409+Georgette-Turner@users.noreply.github.com>
2020-05-22 17:46:44 -0400

    Second Push

--- main.js
+++ main.js
@@ -1,7 +1,18 @@
 const rollButton = document.querySelector('#roll-button')
+const  rollNumber = document.querySelector('#number-of-dice')
 
 const dieRolls = []
 
-rollButton.addEventListener("click", function)
-
-
+rollButton.addEventListener("click", function(){
+    console.log("roll-button")
+    let numberOfrolls = rollNumber.value
+    console.log (numberOfrolls)
+})
+function rollDice(){
+    var die1 = document.getElementById("die1");
+    var status = document.getElementById("status");
+    var rollTotal =d1
+    var d1 = Math.floor(Math.random() * 6) + 1;
+    die1.innerHTML = d1;
+        status.innerHTML = "Total rolled "+numberRolled.value
+}
