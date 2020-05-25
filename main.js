9700efa33abda51cdfb06cf5e2e0a0cc10a630e4
Georgette-Turner <63122409+Georgette-Turner@users.noreply.github.com>
2020-05-24 23:47:53 -0400

    Working in Pregress

--- main.js
+++ main.js
@@ -1,18 +1,24 @@
 const rollButton = document.querySelector('#roll-button')
-const  rollNumber = document.querySelector('#number-of-dice')
+const rollNumber = document.querySelector('#number-of-dice')
 
 const dieRolls = []
 
-rollButton.addEventListener("click", function(){
+rollButton.addEventListener("click", function () {
     console.log("roll-button")
     let numberOfrolls = rollNumber.value
-    console.log (numberOfrolls)
-})
-function rollDice(){
-    var die1 = document.getElementById("die1");
-    var status = document.getElementById("status");
-    var rollTotal =d1
-    var d1 = Math.floor(Math.random() * 6) + 1;
-    die1.innerHTML = d1;
-        status.innerHTML = "Total rolled "+numberRolled.value
-}
+    console.log(numberOfrolls)
+
+    let counter = 0
+    while (counter < numberOfrolls) {
+        var d1 = Math.floor(Math.random() * 6) + 1;
+        console.log(d1)
+        counter += 1
+        function dieRoll() {
+            var result = [];
+            for (var d1 = 0; d1 < 5;
+                d1++)
+                result.push("&#x268" + Math.floor(Math.random() * 6) + ";" );
+                };
+return result;  
+            }
+        })
