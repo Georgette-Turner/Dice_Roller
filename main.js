0a91781f4c5afc56aab78d49bff259daaea8a1a1
Georgette-Turner <63122409+Georgette-Turner@users.noreply.github.com>
2020-06-17 12:37:16 -0400

    3rd Work in Progress

--- main.js
+++ main.js
@@ -1,6 +1,8 @@
 const rollButton = document.querySelector('#roll-button')
 const rollNumber = document.querySelector('#number-of-dice')
-
+const totalSpanElement= document.querySelector('#resultRolled')
+const showAllButton = document.querySelector('#showAll')
+const rollOutput = document.querySelector('#rollOutput')
 const dieRolls = []
 
 rollButton.addEventListener("click", function () {
@@ -9,28 +11,13 @@ rollButton.addEventListener("click", function () {
     console.log(numberOfrolls)
 
     let counter = 0
+    let total = 0
     while (counter < numberOfrolls) {
         var d1 = Math.floor(Math.random() * 6) + 1;
         console.log(d1)
         counter += 1
-        function dieRoll() {
-            var result = [];
-            for (var d1 = 0; d1 < 5;               d1++)
-                result.push("&#x268" + Math.floor(Math.random() * 6) + ";");
-        };var result = dieRoll();
-        counts[result] = counts[result] +1
-        return result;
+     d1 + total   
 
-        console.log(results);
-        console.log(counts)
-        console.log('The loop is over.')
-let showAll = document.querySelector('#show-All');
-let diceRollInput = document.querySelector('#diceRollinput');
-let numberOfDiceRoll = document.querySelector('number-of-dice-roll');
-let letsRoll = document.querySelector('#lets-roll');
-
-var dieRolls = []
-
+        
+        
     }
-}
-}
