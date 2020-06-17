5109cefbd82abd94cbb6d77f619d79aeb0d90ad3
Georgette-Turner <63122409+Georgette-Turner@users.noreply.github.com>
2020-06-17 09:26:41 -0400

    2nd commit

--- main.js
+++ main.js
@@ -15,15 +15,22 @@ rollButton.addEventListener("click", function () {
         counter += 1
         function dieRoll() {
             var result = [];
-            for (var d1 = 0; d1 < 5;
-                d1++)
+            for (var d1 = 0; d1 < 5;               d1++)
                 result.push("&#x268" + Math.floor(Math.random() * 6) + ";");
-        };
+        };var result = dieRoll();
+        counts[result] = counts[result] +1
         return result;
-        console.log('The loop is over.')
-        
 
-        }
+        console.log(results);
+        console.log(counts)
+        console.log('The loop is over.')
+let showAll = document.querySelector('#show-All');
+let diceRollInput = document.querySelector('#diceRollinput');
+let numberOfDiceRoll = document.querySelector('number-of-dice-roll');
+let letsRoll = document.querySelector('#lets-roll');
+
+var dieRolls = []
+
     }
-})
- 
+}
+}
