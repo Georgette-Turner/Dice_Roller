218af81b2028d14e79fe4a0ed3aff40e28d94f27
Georgette-Turner <63122409+Georgette-Turner@users.noreply.github.com>
2020-06-23 22:00:54 -0400

    commit revision

--- main.js
+++ main.js
@@ -1,9 +1,7 @@
 const rollButton = document.querySelector('#roll-button')
 const rollNumber = document.querySelector('#number-of-dice')
-const totalSpanElement= document.querySelector('#resultRolled')
+const totalSpanElement = document.querySelector('#resultRolled')
 const showAllButton = document.querySelector('#showAll')
-const rollOutput = document.querySelector('#rollOutput')
-const dieRolls = []
 
 rollButton.addEventListener("click", function () {
     console.log("roll-button")
@@ -14,7 +12,25 @@ rollButton.addEventListener("click", function () {
     let total = 0
     while (counter < numberOfrolls) {
         var d1 = Math.floor(Math.random() * 6) + 1;
-        console.log(d1)
-        counter += 1
+
+        rollNumber.push(d1)
+        total = total + (d1)
     }
-rollOutput
+    console.log(d1)
+    counter += 1
+
+})
+showAllButton.addEventListener("click", function () {
+    console.log("showAllButton")
+    // while statement
+    let counter = 0
+    let total = 0
+    while (counter = 0) counter < rollNumber.length; counter +=
+        1;
+
+    let showAllButton = "<li class='dice'>+rollNumber[counter +"/li>''
+    totalSpanElement.innerHTML += listElement
+})
+
+
+//  while loop show all button
